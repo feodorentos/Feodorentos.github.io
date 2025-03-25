7153ac56eeda26b4805583ba77f4aa5c006a4074
feodorentos <feodorkuhar@gmail.com>
2025-03-25 22:07:59 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,75 @@
-const nav = document.querySelector('#nav');
-const navBtn = document.querySelector('#nav-btn');
-const navBtimImg = document.querySelector('#nav-btn-img');
+// Карусель отзывы
 
-navBtn.onclick = () => {
-    if (nav.classList.toggle('open')) {
-        navBtimImg.src = "static/images/nav-close.svg"
+let carouselItems = document.querySelectorAll('.carousel-item');
+let dots = document.querySelectorAll('.dot');
+let prevButton = document.querySelector('.prev');
+let nextButton = document.querySelector('.next');
+let currentSlide = 0;
+
+prevButton.addEventListener('click', () => {
+    showPrevSlide();
+});
+
+nextButton.addEventListener('click', () => {
+    showNextSlide();
+});
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        showSlide(index);
+    });
+});
+
+function showSlide(n) {
+    carouselItems.forEach(item => item.classList.remove('active'));
+    dots.forEach(dot => dot.classList.remove('active'));
+    carouselItems[n].classList.add('active');
+    dots[n].classList.add('active');
+    currentSlide = n;
+}
+
+function showNextSlide() {
+    if (currentSlide < carouselItems.length - 1) {
+        showSlide(currentSlide + 1);
     } else {
-        navBtimImg.src = "static/images/nav-open.svg"
-
+        showSlide(0);
     }
 }
+
+function showPrevSlide() {
+    if (currentSlide > 0) {
+        showSlide(currentSlide - 1);
+    } else {
+        showSlide(carouselItems.length - 1);
+    }
+}
+
+
+
+
+
+
+
+// header button
+
+
+const navMedia = document.querySelector('#nav-media');
+const navBtn = document.querySelector('#nav-btn');
+const navCross = document.querySelector('#nav-cross');
+
+
+
+navBtn.addEventListener('click', function() {
+    navMedia.style.display = 'flex'; // Показываем элемент
+});
+
+navCross.addEventListener('click', function() {
+    navMedia.style.display = 'none'; // Скрываем элемент
+});
+
+
+
+
+AOS.init({
+    once: true
+});
